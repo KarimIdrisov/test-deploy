a2fb81338fee326e8c98c189f4114215b45ab2da
iki <karim.idrisow2016@gmail.com>
2022-06-23 00:49:18 +1000

    tss

---
+++ src/api.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import user_routes from "./routes/user.route.js";
+import task_routes from "./routes/task.route.js";
+
+const app = express();
+const port = process.env.PORT || 3500;
+const router = express.Router();
+
+// Настройка CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+user_routes(app);
+task_routes(app);
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
